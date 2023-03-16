638224412ed7bf47a6a8a7ed930328295f1e16bb
Ethan Duggan <eduggan1997@gmail.com>
2023-03-16 16:43:51 -0400

    completed exercise 3.3

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ let persons = [
     }
 ]
 
-//server initialization
+//server setup & initialization
 const express = require('express')
 
 const app = express()
@@ -32,13 +32,33 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
 //routes
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
-
 app.get('/info', (request, response) => {
     response.send(`
         <p>Phonebook has info for ${persons.length} people</p>
         <p>${new Date()}</p>
     `)
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.statusMessage = `Couldn't find person with id=${id}`
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
 })
