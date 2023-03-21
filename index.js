02319ef06d5831ad49bcc22ab01ae143d38355c7
Ethan Duggan <eduggan1997@gmail.com>
2023-03-21 15:58:42 -0400

    completed exercise 3.13

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
-//data
+//import environment variables
+require('dotenv').config()
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-//server setup & initialization
+//import modules
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 
+//import mongoose models
+const Person = require('./models/person')
+
+
+//server initialization
 const app = express()
 
 app.use(cors())
@@ -48,8 +31,9 @@ app.use(morgan((tokens, req, res) => {
     return returnTokens.join(' ')
 }))
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
 
 //routes
 
@@ -61,7 +45,9 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.post('/api/persons', (request, response) => {
@@ -92,16 +78,9 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
+    Person.findById(request.params.id).then(person => {
         response.json(person)
-    } else {
-        response.statusMessage = `Couldn't find person with id=${id}`
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -110,6 +89,7 @@ app.delete('/api/persons/:id', (request, response) => {
 
     response.status(204).end()
 })
+
 
 //helper functions
 
