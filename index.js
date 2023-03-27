651cfc881e02497005b3259c30d8b9232c4b1673
Ethan Duggan <eduggan1997@gmail.com>
2023-03-27 16:50:15 -0400

    completed exercise 3.19

--- index.js
+++ index.js
@@ -58,19 +58,6 @@ app.get('/api/persons', (request, response, next) => {
 
 app.post('/api/persons', (request, response, next) => {
     const body = request.body
-
-    if (!body.name || !body.number) {
-        response.status(400).json({
-            error: 'Request body must contain name and number'
-        })
-        return
-    }
-    /*if (persons.find(person => person.name === body.name)) {
-        response.status(400).json({
-            error: `Person with name ${body.name} already exists`
-        })
-        return
-    }*/
     
     const newPerson = new Person({
         name: body.name,
@@ -109,7 +96,11 @@ app.put('/api/persons/:id', (request, response, next) => {
         number: body.number,
     }
 
-    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    Person.findByIdAndUpdate(
+        request.params.id,
+        person,
+        {new: true, runValidators: true, context: 'query'}
+    )
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -124,6 +115,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
     }
 
     next(error)
